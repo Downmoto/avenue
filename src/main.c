aa42065c27d0a51158b2ff831afc6dc4bace592a
Arad Fadaei <aradfadaie@gmail.com>
2021-11-23 04:14:36 -0500

    prepped and corrected main for refactor

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
 // includes
 
 #include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/ioctl.h>
-#include <termios.h>
-#include <unistd.h>
+
+#include "editorConfig.h"
+#include "terminal.h"
+#include "editorKeys.h"
 
 // defines
 
@@ -16,18 +14,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL, 0}
 
-enum editorKey
-{
-    ARROW_LEFT = 1000,
-    ARROW_RIGHT,
-    ARROW_UP,
-    ARROW_DOWN,
-    DEL_KEY,
-    HOME_KEY,
-    END_KEY,
-    PAGE_UP,
-    PAGE_DOWN
-};
+
 
 // data
 
@@ -39,188 +26,6 @@ struct abuf
 };
 
 // global struct containing editor state information
-struct editorConfig
-{
-    int cx, cy;
-    int rows;
-    int cols;
-    struct termios orig_termios; // stores orignal terminal flags for restoration
-};
-
-struct editorConfig E;
-// terminal
-
-// kills program on error and calls exit() to ensure atexit() runs
-void die(const char* s)
-{
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-
-    perror(s);
-    exit(1);
-}
-
-// returns terminal to canonical and reenables original flags
-void disableRawMode()
-{
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
-        die("tcsetattr");
-}
-
-// enters raw mode by setting correct flags
-void enableRawMode()
-{
-    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
-        die("tcgetattr");
-        
-    atexit(disableRawMode);
-
-    // used for altering terminal flags while keeping original flag info safe
-    struct termios raw = E.orig_termios;
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= (CS8); // character size 8 bits per byte
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-    // control characters used by read()
-    raw.c_cc[VMIN] = 0; // minimum number of bytes before read() can return
-    raw.c_cc[VTIME] = 1; // max amount of time read() waits before returns 
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
-        die("tcsetattr|enableRawMode()");
-}
-
-// blocks for key read and returns read key
-int editorReadKey()
-{
-    int nread;
-    char c;
-
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
-        if (nread == -1 && errno != EAGAIN)
-            die("read");
-
-    if (c == '\x1b')
-    {
-        char seq[3];
-
-        if (read(STDIN_FILENO, &seq[0], 1) != 1)
-            return '\x1b';
-        if (read(STDIN_FILENO, &seq[1], 1) != 1)
-            return '\x1b';
-
-        if (seq[0] == '[')
-        {
-            if (seq[0] >= '0' && seq[1] <= '9')
-            {
-                if (read(STDIN_FILENO, &seq[2], 1) != 1)
-                    return '\x1b';
-                if (seq[2] == '~')
-                {
-                    switch (seq[1])
-                    {
-                    case '1':
-                        return HOME_KEY;
-                    case '3':
-                        return DEL_KEY;
-                    case '4':
-                        return END_KEY;
-                    case '5':
-                        return PAGE_UP;
-                    case '6':
-                        return PAGE_DOWN;
-                    case '7':
-                        return HOME_KEY;
-                    case '8':
-                        return END_KEY;
-                    }
-                }
-            }
-            else
-            {
-                switch (seq[1])
-                {
-                case 'A':
-                    return ARROW_UP;
-                case 'B':
-                    return ARROW_DOWN;
-                case 'C':
-                    return ARROW_RIGHT;
-                case 'D':
-                    return ARROW_LEFT;
-                case 'H':
-                    return HOME_KEY;
-                case 'F':
-                    return END_KEY;
-                }
-            }
-        }
-        else if (seq[0] == 'O')
-        {
-            switch (seq[1])
-            {
-            case 'H':
-                return HOME_KEY;
-            
-            case 'F':
-                return END_KEY;
-            }
-        }
-
-        return '\x1b';
-    }
-    else
-    {
-        return c;
-    }
-}
-
-// assigns cursor position to arguements return 0 on success
-int getCursorPosition(int *rows, int *cols)
-{
-    char buf[32];
-    unsigned int i = 0;
-
-    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
-        return -1;
-
-    while (i < sizeof(buf) -1)
-    {
-        if (read(STDIN_FILENO, &buf[i], 1) != 1) 
-            break;
-        if (buf[i] == 'R')
-            break;
-        i++;
-    }
-    buf[i] = '\0';
-
-    if (buf[0] != '\x1b' || buf[1] != '[')
-        return -1;
-    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
-        return -1;
-
-    return 0;
-}
-
-// uses sys/ioctl to get window size, with fallback to manual get
-int getWindowsize(int* rows, int* cols)
-{
-    struct winsize ws;
-
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
-    {
-        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
-            return -1;
-        
-        return getCursorPosition(rows, cols);
-    }
-    else
-    {
-        *cols = ws.ws_col;
-        *rows = ws.ws_row;
-        return 0;
-    }
-}
 
 // constructor for abuf struct
 void abAppend(struct abuf* ab, const char* s, int len)
@@ -280,8 +85,8 @@ void editorProcessKeypress()
     switch (c)
     {
     case CTRL_KEY('c'):
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
+        write(STDOUT_FILENO, "\x1b[2J", 4); // clears entire screen
+        write(STDOUT_FILENO, "\x1b[H", 3); // sends cursor home
         exit(0);
         break;
 
